1bff4b1dd19117a13732773cd6a50e331c352fc0
louisechristinelimi <louisechristine.lim@gmail.com>
2024-03-29 01:15:49 +0800

    React HW 8

---
+++ src/components/ContactList/ContactListItem/ContactListItem.jsx
@@ -0,0 +1,24 @@
+import css from './ContactListItem.module.css';
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { deleteContact } from '../../../redux/contacts/contactsOperation';
+
+export const ContactListItem = ({ filteredContact }) => {
+  const dispatch = useDispatch();
+
+  const handleDelete = () => {
+    dispatch(deleteContact(filteredContact.id));
+  };
+
+  return (
+    <li className={css.contactListItem}>
+      <p>{filteredContact.name}:</p>
+      <p>{filteredContact.number}</p>
+      <button onClick={handleDelete}>Delete</button>
+    </li>
+  );
+};
+
+ContactListItem.propTypes = {
+  filteredContact: PropTypes.object.isRequired,
+};
